ec235a9a345d79ceca0cf70768fef618ddae480d
Paul Pauls <mail@paulpauls.de>
2016-05-04 10:29:48 +0200

    Reworked encryption and added decryption to demo

--- demo/demo_aes256_ecb.c
+++ demo/demo_aes256_ecb.c
@@ -1,23 +1,21 @@
-#include <stdio.h>  // printf()
-#include <stdlib.h> // ERROR_SUCCESS
-#include <stdint.h> // uint8_t
+#include <stdio.h>   // printf()
+#include <stdlib.h>  // ERROR_SUCCESS
+#include <stdint.h>  // uint8_t
+#include <stdbool.h> // true, false
 
-#include "aes256_ecb.h" // aes256_ecb_encrypt()
+#include "aes256_ecb.h" // aes256_ecb_encrypt(), aes256_ecb_decrypt()
 
 
 
 int main();
+void plain_cipher_print_cmp(const uint8_t *plaintext, size_t plainSize,
+	const uint8_t *key, size_t keySize, int nr);
 void print_hex(const uint8_t *hex, size_t size);
 
 
 
 /* Demo of the aes256 ECB mode, which uses the key and the plaintexts defined
  * as test vectors in 'NIST SP 800-38A' document.
- *
- * Process:
- * 	[1] Define key and plaintexts, declare ciphertexts
- *	[2] Encrypt all plaintexts with key to corresponding ciphertexts
- *	[3] Print key and all plaintexts with corresponding ciphertexts
  */
 int main()
 {
@@ -41,52 +39,13 @@ int main()
 		0xe5, 0xfb, 0xc1, 0x19, 0x1a, 0x0a, 0x52, 0xef,
 		0xf6, 0x9f, 0x24, 0x45, 0xdf, 0x4f, 0x9b, 0x17,
 		0xad, 0x2b, 0x41, 0x7b, 0xe6, 0x6c, 0x37, 0x10};
-	uint8_t ciphertext1[16];
-	uint8_t ciphertext2[16];
-	uint8_t ciphertext3[16];
-	uint8_t ciphertext4[16];
-	uint8_t ciphertext5[64];
 
 
-	aes256_ecb_encrypt(plaintext1, sizeof(plaintext1), key, sizeof(key),
-		ciphertext1, sizeof(ciphertext1));
-	aes256_ecb_encrypt(plaintext2, sizeof(plaintext2), key, sizeof(key),
-		ciphertext2, sizeof(ciphertext2));
-	aes256_ecb_encrypt(plaintext3, sizeof(plaintext3), key, sizeof(key),
-		ciphertext3, sizeof(ciphertext3));
-	aes256_ecb_encrypt(plaintext4, sizeof(plaintext4), key, sizeof(key),
-		ciphertext4, sizeof(ciphertext4));
-	aes256_ecb_encrypt(plaintext5, sizeof(plaintext5), key, sizeof(key),
-		ciphertext5, sizeof(ciphertext5));
-
-
-	printf("Key:\n");
-	print_hex(key, sizeof(key));
-
-	printf("\nPlaintext1:\n");
-	print_hex(plaintext1, sizeof(plaintext1));
-	printf("\nCiphertext1:\n");
-	print_hex(ciphertext1, sizeof(ciphertext1));
-
-	printf("\nPlaintext2:\n");
-	print_hex(plaintext2, sizeof(plaintext2));
-	printf("\nCiphertext2:\n");
-	print_hex(ciphertext2, sizeof(ciphertext2));
-
-	printf("\nPlaintext3:\n");
-	print_hex(plaintext3, sizeof(plaintext3));
-	printf("\nCiphertext3:\n");
-	print_hex(ciphertext3, sizeof(ciphertext3));
-
-	printf("\nPlaintext4:\n");
-	print_hex(plaintext4, sizeof(plaintext4));
-	printf("\nCiphertext4:\n");
-	print_hex(ciphertext4, sizeof(ciphertext4));
-
-	printf("\nPlaintext5:\n");
-	print_hex(plaintext5, sizeof(plaintext5));
-	printf("\nCiphertext5:\n");
-	print_hex(ciphertext5, sizeof(ciphertext5));
+	plain_cipher_print_cmp(plaintext1, sizeof(plaintext1), key, sizeof(key), 1);
+	plain_cipher_print_cmp(plaintext2, sizeof(plaintext2), key, sizeof(key), 2);
+	plain_cipher_print_cmp(plaintext3, sizeof(plaintext3), key, sizeof(key), 3);
+	plain_cipher_print_cmp(plaintext4, sizeof(plaintext4), key, sizeof(key), 4);
+	plain_cipher_print_cmp(plaintext5, sizeof(plaintext5), key, sizeof(key), 5);
 
 
     return EXIT_SUCCESS;
@@ -94,9 +53,62 @@ int main()
 
 
 
+/* Encrypts given plaintext with key, prints them and then compares decrypted
+ * ciphertext with given plaintext
+ *
+ * Process:
+ * 	[1] Define arrays for ciphertext and decryptedCiphertext
+ *	[2] Encrypt and decrypt
+ *	[3] Print results with a final comparison of encryption and decryption
+ */
+void plain_cipher_print_cmp(const uint8_t *plaintext, size_t plainSize,
+	const uint8_t *key, size_t keySize, int nr)
+{
+	uint8_t ciphertext[plainSize];
+	uint8_t decryptedCiphertext[sizeof(ciphertext)];
+	int cmpCorrect = true;
+
+	aes256_ecb_encrypt(plaintext, plainSize, key, keySize, ciphertext,
+		sizeof(ciphertext));
+	aes256_ecb_decrypt(ciphertext, sizeof(ciphertext), key, keySize,
+		decryptedCiphertext, sizeof(decryptedCiphertext));
+
+	printf("****************************************\n");
+	printf("Key:\n");
+	print_hex(key, sizeof(key));
+
+	printf("\nPlaintext%d:\n", nr);
+	print_hex(plaintext, plainSize);
+	printf("\nCiphertext%d:\n", nr);
+	print_hex(ciphertext, sizeof(ciphertext));
+
+	printf("\nPlaintext%d equals decrypt(Ciphertext%d): ", nr, nr);
+
+	// Altought technically unnecessary as the size of decryptedCiphertext
+	// is practically defined with plainSize, it is generally good practice.
+	// Significant speedup with goto possible!
+	if (plainSize != sizeof(decryptedCiphertext))
+		cmpCorrect = false;
+
+	for (int i = 0; i < plainSize; i++) {
+		if(plaintext[i] != decryptedCiphertext[i])
+			cmpCorrect = false;
+	}
+
+	if (cmpCorrect)
+		printf("YES!\n");
+	else
+		printf("NO!\n");
+
+	printf("****************************************\n");
+}
+
+
+
 /* Input:
  *	uint8_t *hex	byte-array consisting of hex-encoded numbers
  *	size_t size	the size of the given byte-array
+ *
  * Output: Prints the hex encoded numbers with a line break each 8th number
  */
 void print_hex(const uint8_t *hex, size_t size)
